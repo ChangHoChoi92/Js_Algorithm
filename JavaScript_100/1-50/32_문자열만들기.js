d7eb00020630ec097f7cc67b21dd4664124e1ca8
ChangHo.Choi <qjqjfl70@gmail.com>
2023-08-16 23:43:55 +0900

    230816_2340

--- JavaScript_100/1-50/32_문자열만들기.js
+++ JavaScript_100/1-50/32_문자열만들기.js
@@ -1,3 +1,15 @@
 // 문제 32 : 문자열 만들기
 // 문자열을 입력받으면 단어의 갯수를 출력하는 프로그램을 작성해 주세요.
 
+function search(string) {
+    let split = string.split("");
+    let count = 0;
+    for (let i = 0; i < split.length; i++) {
+        if (split[i] === " ") {
+            count++;
+        }
+    }
+    return console.log(count);
+}
+
+search("안녕하세요. 저는 제주대학교 컴퓨터공학전공 최창호 입니다.")
