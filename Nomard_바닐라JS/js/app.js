318e6a9416e41b831bbcd7150b6240865c119d57
ChangHo.Choi <qjqjfl70@gmail.com>
2023-09-19 11:46:23 +0900

    timer

--- Nomard_바닐라JS/js/app.js
+++ Nomard_바닐라JS/js/app.js
@@ -13,5 +13,34 @@ function onLoginSubmit(event) {
 
 loginForm.addEventListener('submit', onLoginSubmit);
 
+const root = document.querySelector("#root");
+const container = document.createElement("div");
+const ir = document.createElement("h1");
+const section = document.createElement("section");
+const secTitle = document.createElement("h2");
+const secImg = document.createElement("img");
+const secCont = document.createElement("p");
+
+container.classList.add("container");
+ir.classList.add("ir");
+ir.textContent = '테스트 페이지';
+
+secTitle.classList.add("section-title");
+secTitle.textContent = '섹션제목';
+
+secImg.classList.add("section-img");
+
+secCont.classList.add("section-contents");
+secCont.textContent = '내용';
+
+
+root.append(container);
+container.append(ir, section);
+section.append(secTitle, secImg, secCont);
+
+
+
+
+
 
 
